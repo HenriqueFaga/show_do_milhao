3a0e131f81636a8cc83188cf1ebe1678de5f9469
HenriqueFaga <39166267+HenriqueFaga@users.noreply.github.com>
2019-11-11 17:31:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app.post('/add-cadastro', function (req, res) {
     res.redirect('/login');
 })
 
-var lista_perguntas_individual = []
+var req.session.lista_perguntas_individual = []
 // var req.session.pergunta_individual_momento = 0
 
 // MENU
@@ -178,6 +178,7 @@ app.get('/menu', function (req, res) {
 // Inicio Show
 app.get('/inicio_show', function (req, res) {
     // zeramos a pergunta do momento quando voltamos pro menu
+    req.session.lista_perguntas_individual = []
     req.session.pergunta_individual_momento = 0
     perguntas.findAll({
         // attributes: [[sequelize.fn('COUNT', sequelize.col('dificuldade')), 'dificuldade']],
@@ -188,9 +189,9 @@ app.get('/inicio_show', function (req, res) {
         limit : 3
     }).then(function(dificuldade){
         console.log(dificuldade)
-        lista_perguntas_individual[0] = dificuldade[0]['dataValues']['id']
-        lista_perguntas_individual[1] = dificuldade[1]['dataValues']['id']
-        lista_perguntas_individual[2] = dificuldade[2]['dataValues']['id']
+        req.session.lista_perguntas_individual[0] = dificuldade[0]['dataValues']['id']
+        req.session.lista_perguntas_individual[1] = dificuldade[1]['dataValues']['id']
+        req.session.lista_perguntas_individual[2] = dificuldade[2]['dataValues']['id']
     })
     perguntas.findAll({
     // attributes: [[sequelize.fn('COUNT', sequelize.col('dificuldade')), 'dificuldade']],
@@ -201,9 +202,9 @@ app.get('/inicio_show', function (req, res) {
         limit : 3
     }).then(function(dificuldade){
         console.log(dificuldade)
-        lista_perguntas_individual[3] = dificuldade[0]['dataValues']['id']
-        lista_perguntas_individual[4] = dificuldade[1]['dataValues']['id']
-        lista_perguntas_individual[5] = dificuldade[2]['dataValues']['id']
+        req.session.lista_perguntas_individual[3] = dificuldade[0]['dataValues']['id']
+        req.session.lista_perguntas_individual[4] = dificuldade[1]['dataValues']['id']
+        req.session.lista_perguntas_individual[5] = dificuldade[2]['dataValues']['id']
     })
     perguntas.findAll({
     // attributes: [[sequelize.fn('COUNT', sequelize.col('dificuldade')), 'dificuldade']],
@@ -214,10 +215,10 @@ app.get('/inicio_show', function (req, res) {
         limit : 4
     }).then(function(dificuldade){
         console.log(dificuldade)
-        lista_perguntas_individual[6] = dificuldade[0]['dataValues']['id']
-        lista_perguntas_individual[7] = dificuldade[1]['dataValues']['id']
-        lista_perguntas_individual[8] = dificuldade[2]['dataValues']['id']
-        lista_perguntas_individual[9] = dificuldade[3]['dataValues']['id']
+        req.session.lista_perguntas_individual[6] = dificuldade[0]['dataValues']['id']
+        req.session.lista_perguntas_individual[7] = dificuldade[1]['dataValues']['id']
+        req.session.lista_perguntas_individual[8] = dificuldade[2]['dataValues']['id']
+        req.session.lista_perguntas_individual[9] = dificuldade[3]['dataValues']['id']
     })    
   res.render('inicio_show')
 })
@@ -235,10 +236,10 @@ app.get('/prox_show', function (req, res) {
 })
 // TELA SHOW DO VITAO
 app.get('/show', function (req, res) {
-    console.log(lista_perguntas_individual)
+    console.log(req.session.lista_perguntas_individual)
     perguntas.findAll({
         where: {
-            id: lista_perguntas_individual[req.session.pergunta_individual_momento]
+            id: req.session.lista_perguntas_individual[req.session.pergunta_individual_momento]
         } 
     }).then(function(pergunta_resposta){
         // resp_correta = pergunta_resposta['perguntas']['dataValues']['pergunta']
@@ -288,8 +289,8 @@ app.get('/show', function (req, res) {
         dificuldade = pergunta_resposta[0]['dataValues']['dificuldade']
         numero_pergunta = req.session.pergunta_individual_momento + 1
         // Aqui definimos a numeracao de pergunta:
-        // lista_perguntas_individual[0] = resp_1
-        // console.log(lista_perguntas_individual)
+        // req.session.lista_perguntas_individual[0] = resp_1
+        // console.log(req.session.lista_perguntas_individual)
         // pergunta_resposta = lista
         // lista[0] = a
         // a['resp_correta'] = resp_correta
@@ -444,8 +445,8 @@ app.get('/show_multi', function (req, res) {
         meu_id = req.session.id_usuario
         console.log(usuario_nome_2, id_usuario_2)
         // Aqui definimos a numeracao de pergunta:
-        // lista_perguntas_individual[0] = resp_1
-        // console.log(lista_perguntas_individual)
+        // req.session.lista_perguntas_individual[0] = resp_1
+        // console.log(req.session.lista_perguntas_individual)
         // pergunta_resposta = lista
         // lista[0] = a
         // a['resp_correta'] = resp_correta
@@ -494,7 +495,7 @@ app.get("/comprar.html", function(req, res){
     res.sendFile(__dirname + "/src/comprar.html")
 })
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/src/login.html")
+    res.render('login')
 })
 // Usando o CSS
 app.use('/css/show.css', express.static(__dirname + "/css/show.css"));
@@ -521,3 +522,4 @@ app.use('/imagens/traz.png', express.static(__dirname + "/imagens/traz.png"));
 // Escutando a porta 8080
 var porta = process.env.PORT || 8080;
 server.listen(porta);
+
