f02c170e81fe659feb9d6d9996509e53d3758f05
HenriqueFaga <39166267+HenriqueFaga@users.noreply.github.com>
2019-11-10 23:27:47 -0300

    Update cadastro_perguntas.js

--- models/cadastro_perguntas.js
+++ models/cadastro_perguntas.js
@@ -76,68 +76,68 @@ module.exports = perguntas
 //     resp_incorreta_3: 'ÁRIES',
 //     dificuldade: 1
 // })
-perguntas.create({
-    pergunta: 'Quantos jogadores um jogo de vôlei reúne na quadra?',
-    resp_correta: 'DOZE',
-    resp_incorreta_1: 'DEZ',
-    resp_incorreta_2: 'OITO',
-    resp_incorreta_3: 'SEIS',
-    dificuldade: 2
-})
-perguntas.create({
-    pergunta: 'Que imperador pôs fogo em Roma?',
-    resp_correta: 'NERO',
-    resp_incorreta_1: 'TRAJANO',
-    resp_incorreta_2: 'BRUTUS',
-    resp_incorreta_3: 'CALÍGULA',
-    dificuldade: 2
-})
-perguntas.create({
-    pergunta: 'Como é chamado quem nasce em Milão, na Itália?',
-    resp_correta: 'MILANÊS',
-    resp_incorreta_1: 'MILISTA',
-    resp_incorreta_2: 'MILANOSO',
-    resp_incorreta_3: 'MILANENSE',
-    dificuldade: 2
-})
-perguntas.create({
-    pergunta: 'Que profissional usa uma ferramenta chamada formão?',
-    resp_correta: 'CARPINTEIRO',
-    resp_incorreta_1: 'RELOJOEIRO',
-    resp_incorreta_2: 'CONFEITEIRO',
-    resp_incorreta_3: 'BOMBEIRO',
-    dificuldade: 2
-})
+// perguntas.create({
+//     pergunta: 'Quantos jogadores um jogo de vôlei reúne na quadra?',
+//     resp_correta: 'DOZE',
+//     resp_incorreta_1: 'DEZ',
+//     resp_incorreta_2: 'OITO',
+//     resp_incorreta_3: 'SEIS',
+//     dificuldade: 2
+// })
+// perguntas.create({
+//     pergunta: 'Que imperador pôs fogo em Roma?',
+//     resp_correta: 'NERO',
+//     resp_incorreta_1: 'TRAJANO',
+//     resp_incorreta_2: 'BRUTUS',
+//     resp_incorreta_3: 'CALÍGULA',
+//     dificuldade: 2
+// })
+// perguntas.create({
+//     pergunta: 'Como é chamado quem nasce em Milão, na Itália?',
+//     resp_correta: 'MILANÊS',
+//     resp_incorreta_1: 'MILISTA',
+//     resp_incorreta_2: 'MILANOSO',
+//     resp_incorreta_3: 'MILANENSE',
+//     dificuldade: 2
+// })
+// perguntas.create({
+//     pergunta: 'Que profissional usa uma ferramenta chamada formão?',
+//     resp_correta: 'CARPINTEIRO',
+//     resp_incorreta_1: 'RELOJOEIRO',
+//     resp_incorreta_2: 'CONFEITEIRO',
+//     resp_incorreta_3: 'BOMBEIRO',
+//     dificuldade: 2
+// })
 
-perguntas.create({
-    pergunta: 'Em qual espécie o macho choca os ovos e a fêmea procura alimento?',
-    resp_correta: 'PINGÜIM',
-    resp_incorreta_1: 'ANDORINHA',
-    resp_incorreta_2: 'PATO SELVAGEM',
-    resp_incorreta_3: 'MARRECO',
-    dificuldade: 3
-})
-perguntas.create({
-    pergunta: 'Em qual país está localizado o Muro das lamentações?',
-    resp_correta: 'ISRAEL',
-    resp_incorreta_1: 'ALEMANHA',
-    resp_incorreta_2: 'BRASIL',
-    resp_incorreta_3: 'VENEZUELA',
-    dificuldade: 3
-})
-perguntas.create({
-    pergunta: 'Qual desses países não fica na Ásia?',
-    resp_correta: 'EGITO',
-    resp_incorreta_1: 'PAQUISTÃO',
-    resp_incorreta_2: 'JAPÃO',
-    resp_incorreta_3: 'TAILÂNDIA',
-    dificuldade: 3
-})
-perguntas.create({
-    pergunta: 'Qual desses astros de filmes de ação é belga?',
-    resp_correta: 'JEAN CLAUDE VAN DAMME',
-    resp_incorreta_1: 'ARNOLD SCHWARZENEGGER',
-    resp_incorreta_2: 'SYLVESTER STALLONE',
-    resp_incorreta_3: 'STEVEN SEAGAL',
-    dificuldade: 3
-})
+// perguntas.create({
+//     pergunta: 'Em qual espécie o macho choca os ovos e a fêmea procura alimento?',
+//     resp_correta: 'PINGÜIM',
+//     resp_incorreta_1: 'ANDORINHA',
+//     resp_incorreta_2: 'PATO SELVAGEM',
+//     resp_incorreta_3: 'MARRECO',
+//     dificuldade: 3
+// })
+// perguntas.create({
+//     pergunta: 'Em qual país está localizado o Muro das lamentações?',
+//     resp_correta: 'ISRAEL',
+//     resp_incorreta_1: 'ALEMANHA',
+//     resp_incorreta_2: 'BRASIL',
+//     resp_incorreta_3: 'VENEZUELA',
+//     dificuldade: 3
+// })
+// perguntas.create({
+//     pergunta: 'Qual desses países não fica na Ásia?',
+//     resp_correta: 'EGITO',
+//     resp_incorreta_1: 'PAQUISTÃO',
+//     resp_incorreta_2: 'JAPÃO',
+//     resp_incorreta_3: 'TAILÂNDIA',
+//     dificuldade: 3
+// })
+// perguntas.create({
+//     pergunta: 'Qual desses astros de filmes de ação é belga?',
+//     resp_correta: 'JEAN CLAUDE VAN DAMME',
+//     resp_incorreta_1: 'ARNOLD SCHWARZENEGGER',
+//     resp_incorreta_2: 'SYLVESTER STALLONE',
+//     resp_incorreta_3: 'STEVEN SEAGAL',
+//     dificuldade: 3
+// })
